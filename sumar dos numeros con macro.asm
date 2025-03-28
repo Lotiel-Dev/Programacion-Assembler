2504e614dee0bc343d73d6bd71722aa9f952ccbd
Lotiel-Dev <karlos817gerceis@gmail.com>
2025-03-28 19:20:30 -0400

    Sumar dos numero con macro

---
+++ sumar dos numeros con macro.asm
@@ -0,0 +1,46 @@
+;sumar dos numeros
+
+saltolinea macro
+    mov ah,2
+    mov dl,13
+    int 21h
+    mov dl,10
+    int 21h
+endm
+
+
+org 100h
+
+
+    call captura
+    mov bl,al 
+    call captura
+    mov bh,al
+    
+    sub bx,3030h
+    add bh,bl
+    
+    
+    saltolinea
+    mov ah,2
+    mov dl,bh
+    add dl,30h
+    int 21h
+fin: int 20h
+
+
+proc captura
+
+ciclo:
+    mov ah,7
+    int 21h
+    cmp al,'0'
+    jb ciclo
+    cmp al,'9'
+    ja ciclo
+    mov ah,2
+    mov dl,al
+    int 21h
+    ret
+captura endp
+
